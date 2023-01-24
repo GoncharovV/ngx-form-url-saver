7174f971e6055ba4fe34b2379679e3b6da627b7c
eduard.kopylov <eduard.kopylov@qoollo.com>
2023-01-24 17:40:05 +0300

    develop: continue test

--- projects/form-url-saver-lib/src/lib/form-url-saver.directive.spec.ts
+++ projects/form-url-saver-lib/src/lib/form-url-saver.directive.spec.ts
@@ -6,99 +6,6 @@ import { FormBuilder, FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { ActivatedRoute, Router } from "@angular/router";
 import { of } from "rxjs";
 
-@Component({
-  selector: 'ngx-test-component',
-  template: `
-  <div>
-    <form [formGroup]="form">
-      <select formControlName="id">
-        <option
-          *ngFor="let id of articlesIds"
-          [value]="id"
-        >
-        </option>
-      </select>
-      <select formControlName="authorId">
-      <option
-        *ngFor="let id of authorsIds"
-        [value]="id"
-      >
-      </option>
-      </select>
-      <input
-        type="text"
-        formControlName="description"
-      >
-      <select formControlName="category">
-      <option
-        *ngFor="let category of articleTypes"
-        [value]="category"
-      >
-      </option>
-      </select>
-    </form>
-  </div>`,
-})
-class TestComponent{
-  public readonly defaultFormValue = {
-    id: undefined,
-    authorId: undefined,
-    title: undefined,
-    description: undefined,
-    type: undefined,
-  } as Article;
-
-  public readonly form = this.fb.group<Article>(this.defaultFormValue);
-
-  public readonly articles = [
-    {
-      id: 1,
-      authorId: 1,
-      title: 'title 1',
-      description: 'description 1',
-      type: ArticleType.Discovery,
-    },
-    {
-      id: 2,
-      authorId: 1,
-      title: 'title 2',
-      description: 'description 2',
-      type: ArticleType.Discovery,
-    },
-    {
-      id: 3,
-      authorId: 2,
-      title: 'title 3',
-      description: 'description 3',
-      type: ArticleType.Science,
-    },
-    {
-      id: 4,
-      authorId: 2,
-      title: 'title 4',
-      description: 'description 4',
-      type: ArticleType.Science,
-    },
-    {
-      id: 5,
-      authorId: 3,
-      title: 'title 5',
-      description: 'description 5',
-      type: ArticleType.Science,
-    },
-  ] as Article [];
-
-  public readonly articlesIds = [1,2,3,4,5];
-  public readonly authorsIds = [1,2,3,4,5];
-  public readonly articleTypes = [ArticleType.Discovery, ArticleType.Discovery];
-
-  constructor(
-    private readonly fb: FormBuilder,
-    private readonly router: Router,
-    private readonly route: ActivatedRoute
-  ){}
-}
-
 interface Article {
   id: number | undefined;
   authorId: number | undefined;
@@ -113,9 +20,172 @@ enum ArticleType {
 }
 
 class ActivatedRouteMock {
-  public readonly queryParams = of({
+  public readonly queryParams = {
+    id: '1',
+    authorId: '1',
+    title: 'title 1',
+    description: 'description 1',
     type: 'Science',
-  });
+  };
+
+  public readonly snapshot = {
+
+    queryParams: {
+      id: '2',
+      authorId: '2',
+      title: 'title 3',
+      description: 'description 1',
+      type: 'Science',
+    }
+
+  };
+}
+
+class ArticleBuilder {
+  constructor(
+    private _id ?: number,
+    private _authorId ?: number,
+    private _title ?:string,
+    private _description ?:string,
+    private _type ?: ArticleType
+  ){}
+
+  public withId(id: number) {
+    this._id = id;
+    return this;
+  }
+
+  public withAuthorId(id: number) {
+    this._authorId = id;
+    return this;
+  }
+
+  public withTitle(title: string) {
+    this._title = title;
+    return this;
+  }
+
+  public withDescription(description: string) {
+    this._description = description;
+    return this;
+  }
+
+  public withType(type: ArticleType) {
+    this._type = type;
+    return this;
+  }
+
+  public build () {
+    return {
+      id: this._id,
+      authorId: this._authorId,
+      title: this._title,
+      description: this._description,
+      type: this._type
+    } as Article;
+  };
+}
+
+
+@Component({
+  selector: 'ngx-test-component',
+  template: `
+  <div>
+    <form ngxFormUrlSaver [formGroup]="form">
+
+     <input
+        type="text"
+        formControlName="id"
+      >
+
+       <input
+        type="text"
+        formControlName="authorId"
+      >
+
+       <input
+        type="text"
+        formControlName="title"
+      >
+
+       <input
+        type="text"
+        formControlName="description"
+      >
+
+      <input
+        type="text"
+        formControlName="type"
+      >
+
+      <button type="button" (click)="createArticle()">Create article</button>
+
+    </form>
+
+    <section>
+      <div *ngFor="let article of articles">
+        <p>id: {{article?.id}}</p>
+        <p>id: {{article?.authorId}}</p>
+        <p>id: {{article?.title}}</p>
+        <p>id: {{article?.description}}</p>
+        <p>id: {{article?.type}}</p>
+      </div>
+    </section>
+  </div>`,
+})
+class TestComponent{
+  public readonly defaultFormValue = {
+    id: undefined,
+    authorId: undefined,
+    title: undefined,
+    description: undefined,
+    type: undefined,
+  } as Article;
+
+  public readonly form = this.fb.group<Article>(this.defaultFormValue);
+
+  public readonly articles = [
+    new ArticleBuilder()
+      .withId(1)
+      .withAuthorId(1)
+      .withTitle('title 1')
+      .withDescription('description 1')
+      .withType(ArticleType.Discovery).build(),
+    new ArticleBuilder()
+      .withId(2)
+      .withAuthorId(1)
+      .withTitle('title 2')
+      .withDescription('description 2')
+      .withType(ArticleType.Discovery).build(),
+    new ArticleBuilder()
+      .withId(3)
+      .withAuthorId(2)
+      .withTitle('title 3')
+      .withDescription('description 3')
+      .withType(ArticleType.Science).build(),
+    new ArticleBuilder()
+      .withId(4)
+      .withAuthorId(2)
+      .withTitle('title 4')
+      .withDescription('description 4')
+      .withType(ArticleType.Science).build(),
+    new ArticleBuilder()
+      .withId(5)
+      .withAuthorId(3)
+      .withTitle('title 5')
+      .withDescription('description 5')
+      .withType(ArticleType.Science).build(),
+  ] as Article [];
+
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly router: Router,
+    private readonly route: ActivatedRoute
+  ){}
+
+  public createArticle() {
+    this.articles.push(this.form.value as Article);
+  }
 }
 
 describe('FormUrlSaverDirective', () => {
@@ -138,11 +208,28 @@ describe('FormUrlSaverDirective', () => {
     router = TestBed.inject(Router);
     route = TestBed.inject(ActivatedRoute);
     component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
 
-  it('should create TestComponent', () => {
-    expect(component).toBeTruthy();
+  describe('Initialization tests', () => {
+
+    it('should create TestComponent', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it(`the initial article length should be equal to 5`, () => {
+      expect(component.articles.length).toEqual(5);
+    })
+
+  });
+
+  describe('ngxFormUrlSaver directive tests', () => {
+    it('the article length should grow by 1 after one creating', () => {
+      const oldLeng = component.articles.length;
+      component.createArticle();
+      expect(component.articles.length - 1).toEqual(oldLeng);
+    });
   });
 
 });
