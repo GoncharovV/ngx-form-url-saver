cc5f17ebf3bff54af7167b06e68022e4e15f4f95
eduard.kopylov <eduard.kopylov@qoollo.com>
2023-02-28 10:12:21 +0300

    4520: The base setup of eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,23 @@
 import { ActivatedRoute } from '@angular/router';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    styleUrls: ['./app.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
+
+    // eslint-disable-next-line id-denylist
     public readonly form = new FormGroup({
         name: new FormControl(''),
         age: new FormControl(0),
     });
 
     constructor(
-        private activatedRoute: ActivatedRoute
+        private readonly activatedRoute: ActivatedRoute,
     ) {
     }
 
